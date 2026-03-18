e7c3797c9c463c7442358184ffbe29690383b244
Viktor Svahn <svahnv@gmail.com>
2026-03-18 16:03:00 +0100

    some changes following testing

--- ml_peg/calc/scaling_pol/calc_scaling_pol.py
+++ ml_peg/calc/scaling_pol/calc_scaling_pol.py
@@ -12,7 +12,8 @@ import numpy as np
 import pytest
 import json
 
-from ml_peg.calcs.utils.utils import download_s3_data
+from ml_peg.calcs.utils.utils import download_s3_data		# PRODUCTION
+from ml_peg.calcs.utils.utils import download_github_data	# TEST
 from ml_peg.models.get_models import load_models
 from ml_peg.models.models import current_models
 
@@ -23,42 +24,47 @@ OUT_PATH = Path(__file__).parent / "outputs"
 
 
 @pytest.mark.parametrize("mlip", MODELS.items())
-def test_lattice_energy(mlip: tuple[str, Any]) -> None:
+def test_scaling_pol(mlip: tuple[str, Any]) -> None:
 	"""
-    Run scaling polarizability of linear organic molecules test.
+	Run scaling polarizability of linear organic molecules test.
 
-    Parameters
-    ----------
-    mlip
-        Name of model use and model to get calculator.
-    """
+	Parameters
+	----------
+	mlip
+		Name of model use and model to get calculator.
+	"""
 	model_name, model = mlip
-    clean_calc = model.get_calculator()
+	clean_calc = model.get_calculator()
 
-    # Download 'linear organic mols' dataset
-    scaling_polarizability_dir = (
-        download_s3_data(
-            key="inputs/electric_field/scaling_polarizability/linear_organic_mols.zip", # I guess we need to upload such a file somewhere
-            filename="scaling_polarizability.zip",
-        )
-        / "scaling_polarizability"
-    )
+	# Download 'linear organic mols' dataset
+	"""
+	## THIS SHOULD BE USED IN PRODUCTION
+	scaling_pol_dir = download_s3_data(
+		filename="ORCA_DATA.zip",
+		key="inputs/electric_field/scaling_pol/ORCA_DATA.zip",
+	)
+	"""
+ 
+	## ONLY FOR TESTING
+	scaling_pol_dir = download_github_data(
+		filename="ORCA_DATA.zip",
+		github_uri="https://github.com/viktorsvahn/teoroo_ML-PEG/raw/refs/heads/main/data/source",
+	)
 
-	mols = read(scaling_polarizability_dir / "ORCA_DATA.xyz",':')	# This filename needs to be adjusted to whatever's in the zipfile above
+	mols = read(scaling_pol_dir/"ORCA_DATA.xyz",':')
 	mol_out = []
 
 	for mol in mols:
-	    calc = copy.deepcopy(clean_calc)
-	    mol.calc = calc
+		calc = copy(clean_calc)
+		mol.calc = calc
 
-	    energy = mol.get_potential_energy()
-	    forces = mol.get_forces()
-	    dipole = np.linalg.norm(calc.results['dipole'])
-	    
-	    mol_out.append(mol)
+		energy = mol.get_potential_energy()	
+		if 'dipole' in calc.results:
+			mol_out.append(mol)
 
-    # Write output structures
-    write_dir = OUT_PATH / model_name
-    write_dir.mkdir(parents=True, exist_ok=True)
-    write(write_dir / "ORCA_DATA.xyz", mol_out)	# This filename needs to be adjusted to whatever's in the zipfile above
+	# Write output structures
+	if len(mol_out) > 0:
+		write_dir = OUT_PATH/model_name
+		write_dir.mkdir(parents=True, exist_ok=True)
+		write(write_dir/"ORCA_DATA.xyz", mol_out)	# This filename needs to be adjusted to whatever's in the zipfile above
 
