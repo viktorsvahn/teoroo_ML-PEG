757ecf374d4bb3b1713f6bdd8c2bd5f439baa58c
viktorsvahn <svahnv@gmail.com>
2026-04-24 15:54:32 +0200

    Separated data set into smaller parts

---
+++ data/test.py
@@ -0,0 +1,17 @@
+from ase.io import read, write
+
+
+atoms = read('ORCA_DATA.xyz', ':')
+
+
+alkanes = []
+cumulenes = []
+for a in atoms:
+	print(a.info['config_type'])
+	if 'alkane' in a.info['config_type']:
+		alkanes.append(a)
+	elif 'cumulene' in a.info['config_type']:
+		cumulenes.append(a)
+
+write('ALKANES.xyz', alkanes)
+write('CUMULENES.xyz', cumulenes)
